54d6b781f5ab5ecd6ed044e30c1e66fa44811783
jhashuva <joshua537.nit@gmail.com>
2021-11-25 17:25:43 +0530

    calc age

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ by = input("Enter your birth year: ")
 
 
 
-if by>cy  :
+if int(by)>cy  :
     print("Give correct birth year")
-elif by<0 :
+elif int(by)<0 :
     print("You were not born yet")
 elif type(int(by))== 'int':
     print("give number only")
